5d042aa44ca8e0efe49053340be0addb429bfbb4
Ryan Clements <ryanclementshax@gmail.com>
2024-04-26 18:16:14 -0400

    completely remove mongodb-memory-server

--- apps/web-client/src/server/db/mongo.ts
+++ apps/web-client/src/server/db/mongo.ts
@@ -40,48 +40,6 @@ async function getUri(): Promise<string> {
   if (typeof uri !== 'string') {
     throw new Error('MONGODB_URI environment variable is not set');
   }
-  const useLocalDb = uri === 'local';
-  if (useLocalDb) {
-    const port = 27017;
-    const uri = `mongodb://127.0.0.1:${port}`;
-    const dbPath = path.join(tmpdir(), 'mantis-todo-db');
-    if (await isPortInUse(27017)) {
-      console.log(`Local database found at uri: ${uri}, dbPath: ${dbPath}`);
-      return uri;
-    }
-    await fs.ensureDir(dbPath);
-    const { MongoMemoryServer } = await import('mongodb-memory-server');
-    await MongoMemoryServer.create({
-      instance: {
-        // Without specifying a dbPath a random path will be selected
-        dbPath,
-        // Without specifying the port a random port will be selected
-        port,
-        // Persists the data to disk
-        storageEngine: 'wiredTiger',
-      },
-    });
-    console.log(`Started local database at uri: ${uri}, dbPath: ${dbPath}`);
-    cachedUri = uri;
-    return uri;
-  } else {
-    cachedUri = uri;
-    return uri;
-  }
-}
-
-function isPortInUse(port: number): Promise<boolean> {
-  return new Promise((resolve, reject) => {
-    const server = net.createServer();
-    server.once('error', (err: NodeJS.ErrnoException) => {
-      server.close();
-      if (err.code != 'EADDRINUSE') return reject(err);
-      resolve(true);
-    });
-    server.once('listening', () => {
-      server.close();
-      resolve(false);
-    });
-    server.listen(port);
-  });
+  cachedUri = uri;
+  return uri;
 }

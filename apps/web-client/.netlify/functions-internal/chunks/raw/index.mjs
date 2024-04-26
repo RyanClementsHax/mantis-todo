2f8d50da3a26c16bc06e8aff33cbc66fce1129b6
Ryan Clements <ryanclementshax@gmail.com>
2024-04-26 16:37:07 -0400

    remove /client

--- apps/web-client/.netlify/functions-internal/chunks/raw/index.mjs
+++
@@ -1,5 +0,0 @@
-// ROLLUP_NO_REPLACE 
- const template = "<!doctype html>\n<html lang=\"en\">\n  <head>\n    <meta charset=\"utf-8\" />\n    <title>Mantis Todo</title>\n    <base href=\"/\" />\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />\n    <link rel=\"icon\" type=\"image/x-icon\" href=\"/assets/favicon-B-QwvwhC.ico\" />\n    <script type=\"module\" crossorigin src=\"/assets/index-txuG3RVX.js\"></script>\n    <link rel=\"modulepreload\" crossorigin href=\"/assets/vendor-BQIRO7H9.js\">\n    <link rel=\"stylesheet\" crossorigin href=\"/assets/index-CoIhWvqZ.css\">\n  </head>\n  <body>\n    <app-root></app-root>\n  </body>\n</html>\n";
-
-export { template as default };
-//# sourceMappingURL=index.mjs.map

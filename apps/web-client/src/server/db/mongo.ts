142a96b082a963deeea9e94b3320bbbf6f5468cd
Ryan Clements <ryanclementshax@gmail.com>
2024-04-30 10:55:49 -0400

    add back in memory server

--- apps/web-client/src/server/db/mongo.ts
+++ apps/web-client/src/server/db/mongo.ts
@@ -3,6 +3,7 @@ import mongoose, {
   type Schema,
   type CompileModelOptions,
 } from 'mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
 import path from 'node:path';
 import { tmpdir } from 'node:os';
 import net from 'node:net';
@@ -14,9 +15,7 @@ export async function getConnection(): Promise<typeof mongoose> {
     return cachedConnection;
   }
   const uri = await getUri();
-  console.log('connecting', { uri });
   const newConnection = await mongoose.connect(uri);
-  console.log('connected');
   cachedConnection = newConnection;
   return newConnection;
 }
@@ -42,6 +41,47 @@ async function getUri(): Promise<string> {
   if (typeof uri !== 'string') {
     throw new Error('MONGODB_URI environment variable is not set');
   }
-  cachedUri = uri;
-  return uri;
+  const useLocalDb = uri === 'local';
+  if (useLocalDb) {
+    const port = 27017;
+    const uri = `mongodb://127.0.0.1:${port}`;
+    const dbPath = path.join(tmpdir(), 'mantis-todo-db');
+    if (await isPortInUse(27017)) {
+      console.log(`Local database found at uri: ${uri}, dbPath: ${dbPath}`);
+      return uri;
+    }
+    await fs.ensureDir(dbPath);
+    await MongoMemoryServer.create({
+      instance: {
+        // Without specifying a dbPath a random path will be selected
+        dbPath,
+        // Without specifying the port a random port will be selected
+        port,
+        // Persists the data to disk
+        storageEngine: 'wiredTiger',
+      },
+    });
+    console.log(`Started local database at uri: ${uri}, dbPath: ${dbPath}`);
+    cachedUri = uri;
+    return uri;
+  } else {
+    cachedUri = uri;
+    return uri;
+  }
+}
+
+function isPortInUse(port: number): Promise<boolean> {
+  return new Promise((resolve, reject) => {
+    const server = net.createServer();
+    server.once('error', (err: NodeJS.ErrnoException) => {
+      server.close();
+      if (err.code != 'EADDRINUSE') return reject(err);
+      resolve(true);
+    });
+    server.once('listening', () => {
+      server.close();
+      resolve(false);
+    });
+    server.listen(port);
+  });
 }
